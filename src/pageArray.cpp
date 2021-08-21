45080aa60c3b289ff13b69fad3b3bae6443bd073
Carlos12001 <carlos.andres12001@gmail.com>
2021-08-21 07:06:54 -0600

    Version 0.1 pageArray.
    This version create the class and the primate attributes.

---
+++ src/pageArray.cpp
@@ -0,0 +1,16 @@
+#include <iostream>;
+
+class PageArray{
+    public:
+    int numbers_count, size;
+        struct Page{
+            int min_value;
+            int max_value;
+            int id;
+        };
+        struct Page mmu[size];
+        string ram [6];
+
+
+
+};
