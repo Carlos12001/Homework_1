837c9f1624b653cbaf186051992175489a47d8cf
Carlos12001 <carlos.andres12001@gmail.com>
2021-08-22 15:21:35 -0600

    Add method getInfoPage in PageArray.
    The method is create and works good.

--- src/fileSort.cpp
+++ src/fileSort.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include "fstream"
 using namespace std;
 
 void insertionSort(int arr[], int n){
@@ -17,8 +19,10 @@ void insertionSort(int arr[], int n){
 
 void printArray(int arr[], int n){
     int i;
-    for (i = 0; i < n; i++)
+    for (i = 0; i < n; i++){
         cout << arr[i] << " ";
+        if (i!=0&& i%10==0) {cout << endl;}
+    }
     cout << endl;
 }
 
@@ -41,20 +45,62 @@ public:
 
 class PageArray{
 public:
-    int numbers_count;
-    int size;
-    Page mmu[12];
-    string ram[6];
+    int n;
+    const int size_page = 256;
+    int **ram = new int*[6];
+    bool full_ram;
+    Page mmu [12];
     string path;
 
     PageArray() = default;
     PageArray(string path){
         this->path = move(path);
     }
+
+private:
+    int * getInfoPage(int multiple_page){
+        fstream my_file;
+
+        string info ="";
+        char index;
+
+        int counter = 0;
+        int *page =  new int [this->size_page];
+
+        my_file.open (this->path, ios :: in);
+
+        if (my_file.is_open()){
+            while(!my_file.eof() && counter < this->size_page * multiple_page){
+                my_file >> index;
+
+                if(index==',')
+                    counter++;
+
+                if(this->size_page * multiple_page - this->size_page <= counter){
+
+                    if (index == '1' || index =='2' || index =='3' ||
+                    index == '4' || index =='5' || index =='6' ||
+                    index == '7' || index =='8' || index =='9' ||
+                    index == '0'){
+                        info += index;
+                        continue;
+                    }
+
+                    else if (index==',' && info!=""){
+                        *(page+((counter-1) % this->size_page)) = stoi(info);
+                        info = "";
+                    }
+
+                    else
+                        continue;
+                }
+            }
+        }
+        my_file.close();
+        return page;
+    }
 };
 
 int main(){
-
     return 0;
 }
-
